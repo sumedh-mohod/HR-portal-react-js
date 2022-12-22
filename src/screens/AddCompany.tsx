ced00f476e9b7539a4c9a946014d740168f1bb42
kajal.nimje <kajal.nimje@orangebitsindia.com>
2022-12-22 15:57:25 +0530

    add-company updated code

--- src/screens/AddCompany.tsx
+++ src/screens/AddCompany.tsx
@@ -1,15 +1,17 @@
 import React from "react";
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
-import Button from "@mui/material/Button";
-import Grid from "@mui/material/Grid";
 import {
+  Box,
+  Container,
+  Button,
+  Grid,
   Checkbox,
   FormControl,
   FormControlLabel,
   FormLabel,
   TextField,
   Typography,
+  Select,
+  MenuItem,
 } from "@mui/material";
 
 const AddCompany = () => {
@@ -25,7 +27,8 @@ const AddCompany = () => {
         }}
       >
         <Typography variant="h5">New Company</Typography>
-        <Button variant="contained"
+        <Button
+          variant="contained"
           sx={{
             backgroundColor: "#F58634",
             borderRadius: "5px",
@@ -34,24 +37,24 @@ const AddCompany = () => {
           Save
         </Button>
       </Box>
-      
+
       {/* form fields started */}
       <Grid container spacing={5} sx={{ padding: "10px 80px" }}>
         <Grid item xs={12} lg={6}>
           <FormControl fullWidth>
-            <FormLabel id="company">Company</FormLabel>
+            <FormLabel id="company">Company*</FormLabel>
             <TextField name="company" variant="filled" size="small" />
           </FormControl>
         </Grid>
         <Grid item xs={12} lg={6}>
           <FormControl fullWidth>
-            <FormLabel id="defaultLetterHead">Default Letter Head</FormLabel>
+            <FormLabel id="defaultLetterHead">Default Letter Head*</FormLabel>
             <TextField name="defaultLetterHead" variant="filled" size="small" />
           </FormControl>
         </Grid>
         <Grid item xs={12} lg={6}>
           <FormControl fullWidth>
-            <FormLabel id="abbr">Abbr</FormLabel>
+            <FormLabel id="abbr">Abbr*</FormLabel>
             <TextField name="abbr" variant="filled" size="small" />
           </FormControl>
         </Grid>
@@ -63,7 +66,7 @@ const AddCompany = () => {
         </Grid>
         <Grid item xs={12} lg={6}>
           <FormControl fullWidth>
-            <FormLabel id="defaultCurrency">Default Currency</FormLabel>
+            <FormLabel id="defaultCurrency">Default Currency*</FormLabel>
             <TextField name="defaultCurrency" variant="filled" size="small" />
           </FormControl>
         </Grid>
@@ -73,10 +76,18 @@ const AddCompany = () => {
             <TextField name="domain" variant="filled" size="small" />
           </FormControl>
         </Grid>
-        <Grid item xs={12} lg={6}>
+        <Grid item  xs={12} lg={6}>
           <FormControl fullWidth>
-            <FormLabel id="country">Country</FormLabel>
-            <TextField name="country" variant="filled" size="small" />
+            <FormLabel id="country">Country*</FormLabel>
+            <Select
+              variant="filled"
+              size="small"
+              labelId="country"
+              id="country"
+              defaultValue="India"
+            >
+              <MenuItem value="India">India</MenuItem>
+            </Select>
           </FormControl>
         </Grid>
         <Grid item xs={12} lg={6}>
@@ -88,6 +99,7 @@ const AddCompany = () => {
               name="dateOfEstablishment"
               variant="filled"
               size="small"
+              type="date"
             />
           </FormControl>
         </Grid>
