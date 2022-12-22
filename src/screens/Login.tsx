8fc9232a0e261d76cb3f27ea116ab424fff518d3
sumedh.mohod <sumedh.mohod@orangebitsindia.com>
2022-12-22 12:42:42 +0530

    Login Page Updated

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,20 +1,13 @@
-import { Box, Container } from "@mui/system";
 import React from "react";
-import background from "../assets/images/Rectangle1.svg"
-import logo from "../assets/images/logo.png"
-import { FormControl } from '@mui/material';
-import TextField from "@mui/material/TextField";
-import { TextFields } from "@mui/icons-material";
-import Button from '@mui/material/Button';
-import InputAdornment from "@mui/material/InputAdornment";
-import FormLabel from "@mui/material/FormLabel";
-import IconButton from '@mui/material/IconButton';
+import IconOrangebits from "../assets/images/IconOrangebits.svg"
+import background from "../assets/images/LoginBackground.svg"
+import { FormControl, Box, Typography, Grid, FormLabel, TextField, Button, InputAdornment, IconButton } from '@mui/material';
 import Visibility from "@mui/icons-material/Visibility";
-import Grid from '@mui/material/Grid';
+
 const Login = () => {
   return <>
     <Box sx={{
-      padding: "15px",
+      padding: "10px",
       display: "flex",
       flexDirection: "column",
       justifyContent: "center",
@@ -23,171 +16,108 @@ const Login = () => {
       backgroundImage: `url(${background})`,
       backgroundRepeat: "no-repeat",
       backgroundSize: "cover",
-      backgroundPosition: "center"
-    }}
-    >
+      backgroundPosition: "center",
+      height: "95vh"
+
+    }} >
       <Box
         sx={{
-          border: "1px solid #DFE0EB;",
           borderRadius: "8px",
           background: "#FFFFFF",
           padding: "20px",
-          height: "max-content",
           marginBottom: "50px",
           marginTop: "20px",
-          maxWidth: "350px"
+          maxWidth: "340px",
         }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} lg={12} sx={{ display: "flex", justifyContent: "center" }}>
-            <img src={logo} width="61px" height="61px" style={{ marginTop: "36px" }} />
-          </Grid>
-          <Grid item xs={12} lg={12} sx={{ display: "flex", justifyContent: "center" }}>
-            <h2 style={{
+        <Grid container spacing={2} sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+          <img src={IconOrangebits} width="61px" height="61px" style={{ margin: "36px auto" }} />
+          <Typography
+            sx={{
               fontWeight: "700",
               fontSize: "18px",
-              letterSpacing: "0.3 px",
               color: "#252733"
-            }}>Login to Orangebits Technologies</h2>
-          </Grid>
-          <Grid item xs={12} lg={12} sx={{ display: "flex", justifyContent: "center" }}>
-            <FormControl sx={{ marginTop: "32px" }} >
+            }}
+            variant="h5" gutterBottom>
+            Login to Orangebits Technologies
+          </Typography>
+          <FormControl sx={{ p: 2, m: 2 }} >
+            <FormLabel id="outlined-password" sx={{
+              fontWeight: "700",
+              fontSize: "12px",
+              color: "#9FA2B4"
+            }}>USERNAME</FormLabel>
+            <TextField fullWidth size="small" id="password" variant="filled" type={"text"}
+              sx={{
+                fontSize: "14px",
+                color: "#4B506D",
+                opacity: 0.4,
+                width: "292px",
+              }} />
+          </FormControl>
+          <FormControl sx={{ p: 2, m: 2 }} >
+            <Box sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              width: "100%",
+              alignItems: "baseline"
+            }}>
               <FormLabel id="outlined-password" sx={{
-                // fontFamily: 'Mulish',
-                fontStyle: "normal",
                 fontWeight: "700",
                 fontSize: "12px",
-                lineHeight: "15px",
-                letterSpacing: "0.3px",
-                textTransform: "uppercase",
-                color: "#9FA2B4",
-                margin: "32px auto",
-                marginBottom: "6px",
-                width: "316px",
-                height: "16px",
-              }}>Username</FormLabel>
-              <TextField
+                color: "#9FA2B4"
+              }}>PASSWORD</FormLabel>
+              <Typography
                 sx={{
-                  fontFamily: 'Mulish',
-                  fontStyle: "normal",
-                  fontWeight: "400",
-                  fontSize: "14px",
-                  lineHeight: "20px",
-                  letterSpacing: "0.3px",
-                  textTransform: "uppercase",
-                  color: "#4B506D",
-                  margin: "6px auto",
-                  opacity: 0.4,
-                  width: "292px",
-                  height: "20px",
-                  border: "none",
-                  outline: "none"
-                }}
-                id="password"
-                variant="filled"
-                type={"text"}
-
-              />
-            </FormControl>
-          </Grid>
-          <Grid item xs={12} lg={12} sx={{ display: "flex", justifyContent: "center" }}>
-            <FormControl sx={{ marginTop: "32px" }} >
-              <Box sx={{
-                display: "flex", justifyContent: "space-between", width: "316px",
-                height: "16px",
-                margin: "auto",
-              }}>
-                <FormLabel id="outlined-password" sx={{
-                  fontFamily: 'Mulish!important',
-                  fontStyle: "normal",
-                  fontWeight: "700",
-                  fontSize: "12px",
-                  lineHeight: "15px",
-                  letterSpacing: "0.3px",
-                  textTransform: "uppercase",
-                  color: "#9FA2B4",
-                  marginTop: "32px"
-                }}>Passowrd</FormLabel>
-                <p style={{
-
-                  fontFamily: 'Mulish',
-                  fontStyle: "normal",
-                  fontWeight: "400",
                   fontSize: "10px",
-                  lineHeight: "13px",
-                  textAlign: "right",
-                  letterSpacing: "0.1px",
                   color: "#9FA2B4",
-                  marginTop: "32px",
-                  marginBottom: "32px"
-
-                }}>Forgot Password ?</p>
-              </Box>
-              <TextField
-                sx={{
-                  fontFamily: 'Mulish',
-                  fontStyle: "normal",
-                  fontWeight: "400",
-                  fontSize: "14px",
-                  lineHeight: "20px",
-                  letterSpacing: "0.3px",
-                  textTransform: "uppercase",
-                  color: "#4B506D",
-                  margin: "36px auto",
-                  opacity: 0.4,
-                  width: "292px",
-                  height: "20px",
-                  border: "none",
-                  outline: "none"
-
                 }}
-                id="outlined-password"
-                variant="filled"
-                type={"password"}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton aria-label="toggle password visibility" edge="end">
-                        <Visibility />
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-
-              />
-
-            </FormControl>
-          </Grid>
-          <Grid item xs={12} lg={12} sx={{ display: "flex", justifyContent: "center" }}>
-            <Button variant="contained"
+                variant="h5" gutterBottom>
+                Forgot Password ?
+              </Typography>
+            </Box>
+            <TextField fullWidth size="small" id="outlined-password" variant="filled" type={"password"}
               sx={{
-                width: "316px",
-                height: "48px",
-                margin: "auto",
-                marginTop: "56px",
-                borderRadius: "8px", backgroundColor: "#F58634",
-                color: "white",
-                "&:hover": {
-                  backgroundColor: "#F58634",
-                  boxShadow: "0px 4px 12px rgba(55, 81, 255, 0.24)",
-                }
-              }}> Log In</Button>
-          </Grid>
-
+                fontSize: "14px",
+                color: "#4B506D",
+                opacity: 0.4,
+                width: "292px",
+              }}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton aria-label="toggle password visibility" edge="end">
+                      <Visibility />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </FormControl>
+          <Button variant="contained"
+            sx={{
+              width: "316px",
+              height: "48px",
+              margin: "auto",
+              borderRadius: "8px",
+              backgroundColor: "#F58634",
+              color: "white",
+              "&:hover": {
+                backgroundColor: "#F58634",
+                boxShadow: "0px 4px 12px rgba(55, 81, 255, 0.24)",
+              }
+            }}> Log In</Button>
         </Grid>
 
       </Box>
       <Grid container spacing={2}>
         <Grid item xs={12} lg={12} sx={{ display: "flex", justifyContent: "center" }}>
-          <h2 style={{
-            fontFamily: 'Mulish',
-            fontStyle: "normal",
-            fontWeight: "600",
-            fontSize: "18px",
-            lineHeight: "23px",
-            textAlign: "center",
-            letterSpacing: "0.3px",
-            color: "white",
-          }}>Orangebits Software Technologies (India) Pvt Ltd</h2>
+          <Typography
+            sx={{
+              fontWeight: "600",
+              fontSize: "18px",
+              color: "white",
+            }}
+            variant="h2" gutterBottom>Orangebits Software Technologies (India) Pvt Ltd</Typography>
         </Grid>
       </Grid>
     </Box>
