58a5e1a44b1df879776818e5d1c7b4d74ba95c63
kajal.nimje <kajal.nimje@orangebitsindia.com>
2022-12-22 11:14:24 +0530

    add-company page updated

--- src/screens/AddCompanies.tsx
+++ src/screens/AddCompanies.tsx
@@ -9,13 +9,14 @@ import {
   FormControlLabel,
   FormLabel,
   TextField,
+  Typography,
 } from "@mui/material";
 
 const AddCompany = () => {
   return (
     <Container sx={{ p: 5 }}>
       <Box
-        style={{
+        sx={{
           display: "flex",
           justifyContent: "space-between",
           padding: "10px 80px",
@@ -23,21 +24,9 @@ const AddCompany = () => {
           alignContent: "flex-start",
         }}
       >
-        <h2
-          style={{
-            fontWeight: "700",
-            fontSize: "20px",
-            letterSpacing: "0.3px",
-            color: "#252733",
-          }}
-        >
-          New Company
-        </h2>
-        <Button
-          variant="contained"
+        <Typography variant="h5">New Company</Typography>
+        <Button variant="contained"
           sx={{
-            width: "90px",
-            height: "32px",
             backgroundColor: "#F58634",
             borderRadius: "5px",
           }}
@@ -45,6 +34,7 @@ const AddCompany = () => {
           Save
         </Button>
       </Box>
+      
       {/* form fields started */}
       <Grid container spacing={5} sx={{ padding: "10px 80px" }}>
         <Grid item xs={12} lg={6}>
@@ -56,7 +46,7 @@ const AddCompany = () => {
         <Grid item xs={12} lg={6}>
           <FormControl fullWidth>
             <FormLabel id="defaultLetterHead">Default Letter Head</FormLabel>
-            <TextField variant="filled" name="defaultLetterHead" size="small" />
+            <TextField name="defaultLetterHead" variant="filled" size="small" />
           </FormControl>
         </Grid>
         <Grid item xs={12} lg={6}>
@@ -74,19 +64,19 @@ const AddCompany = () => {
         <Grid item xs={12} lg={6}>
           <FormControl fullWidth>
             <FormLabel id="defaultCurrency">Default Currency</FormLabel>
-            <TextField variant="filled" name="defaultCurrency" size="small" />
+            <TextField name="defaultCurrency" variant="filled" size="small" />
           </FormControl>
         </Grid>
         <Grid item xs={12} lg={6}>
           <FormControl fullWidth>
             <FormLabel id="domain">Domain</FormLabel>
-            <TextField variant="filled" name="Domain" size="small" />
+            <TextField name="domain" variant="filled" size="small" />
           </FormControl>
         </Grid>
         <Grid item xs={12} lg={6}>
           <FormControl fullWidth>
             <FormLabel id="country">Country</FormLabel>
-            <TextField variant="filled" name="country" size="small" />
+            <TextField name="country" variant="filled" size="small" />
           </FormControl>
         </Grid>
         <Grid item xs={12} lg={6}>
@@ -95,8 +85,8 @@ const AddCompany = () => {
               Date of Establishment
             </FormLabel>
             <TextField
-              variant="filled"
               name="dateOfEstablishment"
+              variant="filled"
               size="small"
             />
           </FormControl>
@@ -112,7 +102,7 @@ const AddCompany = () => {
         <Grid item xs={12} lg={6}>
           <FormControl fullWidth>
             <FormLabel id="parentsCompany">Parents Company</FormLabel>
-            <TextField variant="filled" name="parentsCompany" size="small" />
+            <TextField name="parentsCompany" variant="filled" size="small" />
           </FormControl>
         </Grid>
         <Grid item xs={12} lg={6}>
@@ -121,8 +111,8 @@ const AddCompany = () => {
               Default Holliday List
             </FormLabel>
             <TextField
-              variant="filled"
               name="defaultHollidayList"
+              variant="filled"
               size="small"
             />
           </FormControl>
