37f7c4c9747b473cd8912af3085065fad0728136
Rijwan Pathan <rijwan.pathan@orangebitsindia.com>
2022-12-22 12:49:03 +0530

    breadcrumb, appbar changes, title and icon

---
+++ src/components/Icons/Arrow.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Arrow = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="12"
+      height="11.498"
+      viewBox="0 0 12 11.498"
+    >
+      <g
+        id="Group_14643"
+        data-name="Group 14643"
+        transform="translate(0.244 -0.405)"
+      >
+        <path
+          id="Path_17062"
+          data-name="Path 17062"
+          d="M40.91,26.751a.923.923,0,0,1-.334.687L35,32.2a1.31,1.31,0,0,1-1.646,0,.892.892,0,0,1,0-1.4l4.759-4.057-4.759-4.057a.892.892,0,0,1,0-1.4,1.311,1.311,0,0,1,1.646,0l0,0,5.58,4.757A.919.919,0,0,1,40.91,26.751Z"
+          transform="translate(-30.849 -20.584)"
+          fill="#2b2a29"
+        />
+        <rect
+          id="Rectangle_168"
+          data-name="Rectangle 168"
+          width="12"
+          height="11"
+          transform="translate(-0.244 0.464)"
+          fill="none"
+        />
+      </g>
+    </svg>
+  );
+};
+
+export default Arrow;
