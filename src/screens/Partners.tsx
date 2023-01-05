c3e98cd3d5ec369f2845122448e08596c475a947
Rijwan Pathan <rijwan.pathan@orangebitsindia.com>
2023-01-05 13:44:38 +0530

    customized buttons

--- src/screens/Partners.tsx
+++ src/screens/Partners.tsx
@@ -1,33 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { DataGrid, GridToolbar, GridColDef } from "@mui/x-data-grid";
-import {
-  Grid,
-  Box,
-  Card,
-  Typography,
-  Container,
-  IconButton,
-  TextField,
-  Menu,
-  MenuItem,
-  ListItemIcon,
-  ListItemText,
-  Paper,
-} from "@mui/material";
+import { GridColDef } from "@mui/x-data-grid";
+import { Grid, Box, Typography, Container, TextField } from "@mui/material";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
 import { getPartners } from "../store/reducers/partners/partners";
 import { useNavigate } from "react-router-dom";
 import { styles } from "../styles/screens/Partners";
 import SearchIcon from "@mui/icons-material/Search";
-import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
-import FormatListBulletedOutlinedIcon from "@mui/icons-material/FormatListBulletedOutlined";
-import ViewColumnOutlinedIcon from "@mui/icons-material/ViewColumnOutlined";
-import PartnersListCard from "../components/GridView/PartnersCard";
-import DragIndicatorIcon from "@mui/icons-material/DragIndicator";
-import AddIcon from "@mui/icons-material/Add";
-import CloseIcon from "@mui/icons-material/Close";
+import PartnersCard from "../components/GridView/PartnersCard";
 import { globalStyles } from "../styles/global";
 import PartnersList from "../components/ListView/PartnersList";
+import CustomizationButtons from "../components/CustomizationButtons";
 
 const columns: GridColDef[] = [
   {
@@ -154,131 +136,17 @@ const Partners = () => {
             display: "flex",
           }}
         >
-          <IconButton
-            sx={{
-              padding: "5px",
-              borderRadius: "5px",
-              border: "solid",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              borderColor: "grey",
-              borderWidth: "thin",
-              mr: "10px",
-              cursor: "pointer",
-            }}
-            disabled={designView === "grid" ? true : false}
-            onClick={() => {
-              setDesignView("grid");
-            }}
-          >
-            <GridViewOutlinedIcon />
-          </IconButton>
-          <IconButton
-            sx={{
-              padding: "5px",
-              borderRadius: "5px",
-              border: "solid",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              borderColor: "grey",
-              borderWidth: "thin",
-              mr: "10px",
-              cursor: "pointer",
-            }}
-            disabled={designView === "list" ? true : false}
-            onClick={() => {
-              setDesignView("list");
-            }}
-          >
-            <FormatListBulletedOutlinedIcon />
-          </IconButton>
-          <IconButton
-            sx={{
-              padding: "5px",
-              borderRadius: "5px",
-              border: "solid",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              borderColor: "grey",
-              borderWidth: "thin",
-              cursor: "pointer",
-            }}
-            id="basic-button"
-            aria-controls={openDropDown ? "basic-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={openDropDown ? "true" : undefined}
-            onClick={handleClickDropDown}
-            disabled={designView === "grid" ? true : false}
-          >
-            <ViewColumnOutlinedIcon />
-          </IconButton>
-          <Menu
-            id="basic-menu"
+          <CustomizationButtons
+            setDesignView={setDesignView}
+            handleClickDropDown={handleClickDropDown}
+            handleCloseDropDown={handleCloseDropDown}
+            handleAddColumn={handleAddColumn}
+            handleRemoveColumn={handleRemoveColumn}
+            designView={designView}
+            openDropDown={openDropDown}
             anchorEl={anchorEl}
-            open={openDropDown}
-            onClose={handleCloseDropDown}
-            MenuListProps={{
-              "aria-labelledby": "basic-button",
-            }}
-            sx={{
-              "& 	.MuiMenu-paper": {
-                width: "22vw",
-                padding: "10px",
-              },
-            }}
-          >
-            {showColumns.map((item) => (
-              <MenuItem
-                sx={{
-                  "&:hover": {
-                    cursor: "default",
-                  },
-                  padding: "5px",
-                  border: "solid",
-                  borderWidth: 1,
-                  borderRadius: "5px",
-                  marginTop: "15px",
-                  borderColor: "#DFDFDF",
-                  backgroundColor: !item.hide ? "#EDEDED" : "white",
-                }}
-              >
-                <ListItemIcon>
-                  <DragIndicatorIcon />
-                </ListItemIcon>
-                <ListItemText
-                  sx={{
-                    textAlign: "start",
-                  }}
-                >
-                  {item.headerName}
-                </ListItemText>
-                <ListItemIcon
-                  sx={{
-                    "&:hover": {
-                      cursor: "pointer",
-                    },
-                  }}
-                >
-                  {item.hide ? (
-                    <AddIcon
-                      onClick={() => {
-                        handleAddColumn(item);
-                      }}
-                    />
-                  ) : (
-                    <CloseIcon
-                      onClick={() => {
-                        handleRemoveColumn(item);
-                      }}
-                    />
-                  )}
-                </ListItemIcon>
-              </MenuItem>
-            ))}
-          </Menu>
+            showColumns={showColumns}
+          />
           <TextField
             sx={{ ml: 2 }}
             size="small"
@@ -300,7 +168,7 @@ const Partners = () => {
             rows={rows}
           />
         ) : (
-          <PartnersListCard
+          <PartnersCard
             partners={rows}
             handlePartnerEditClick={handlePartnerEditClick}
             handlePartnerAddClick={handlePartnerAddClick}
