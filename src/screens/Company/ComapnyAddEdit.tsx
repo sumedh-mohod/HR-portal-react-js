916f45887ff40b05e4d4871a93ac073928f37171
sumedh.mohod <sumedh.mohod@orangebitsindia.com>
2023-01-13 18:59:43 +0530

    Leave policy page done

--- src/screens/Company/ComapnyAddEdit.tsx
+++ src/screens/Company/ComapnyAddEdit.tsx
@@ -3,6 +3,7 @@ import AddCompany from "../../components/Company/AddCompany";
 import EditCompany from "../../components/Company/EditCompany";
 
 import { useParams, useLocation } from "react-router-dom";
+import LeavePolicy from "./LeavePolicy";
 
 const CompanyAddEdit = () => {
   let params = useParams();
@@ -10,7 +11,7 @@ const CompanyAddEdit = () => {
   // console.log("company data ", location.state.company);
 
   const { opration } = params;
-  return <>{opration === "add" ? <AddCompany /> : <EditCompany />}</>;
+  return <>{opration === "add" ? <LeavePolicy /> : <EditCompany />}</>;
 };
 
 export default CompanyAddEdit;
