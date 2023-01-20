3a4cd84c04d3d9dac9cf0dd98d3fca3fe57a3a8b
sumedh.mohod <sumedh.mohod@orangebitsindia.com>
2023-01-20 17:58:52 +0530

    company details added with address card

--- src/components/Company/CompanyAddressDetailsCard.tsx
+++ src/components/Company/CompanyAddressDetailsCard.tsx
@@ -1,38 +1,39 @@
+import React from 'react'
 import { Card, Grid, Typography, Box } from '@mui/material'
 import { styles } from "../../styles/components/editCompany";
-import React from 'react'
 
-const CompanyAddressDetailsCard = () => {
+interface CompanyAddressDetailsCardInterface {
+    info: any,
+}
+const CompanyAddressDetailsCard = (props: CompanyAddressDetailsCardInterface) => {
+    const { info } = props;
+    console.log("info", info);
     return (
-        <Card sx={{ mt: 3, mb: 3, p: 5 }}>
-            <Grid container spacing={3}>
-                <Box {...styles.detailsParentBox} >
-                    <Grid item xs={12} md={2} lg={4}>
-                        <Typography {...styles.detailsTypography1}>Address Line 1 -</Typography>
-                        <Typography {...styles.detailsTypography1}>Address Line 2 -</Typography>
-                        <Typography {...styles.detailsTypography1}>Country -</Typography>
-                        <Typography {...styles.detailsTypography1}>State -</Typography>
-                        <Typography {...styles.detailsTypography1}>City -</Typography>
-                        <Typography {...styles.detailsTypography1}>Postal Code -</Typography>
-                    </Grid>
-                    <Grid item xs={12} md={10} lg={8}>
-                        {data.companyAddresses.map((info: any) => {
-                            return (
-                                <>
-                                    <Typography {...styles.detailsTypography2}> {info.line1}</Typography>
-                                    <Typography {...styles.detailsTypography2}> {info.line2} </Typography>
-                                    <Typography {...styles.detailsTypography2}> {info.country} </Typography>
-                                    <Typography {...styles.detailsTypography2}> {info.state} </Typography>
-                                    <Typography {...styles.detailsTypography2}> {info.city} </Typography>
-                                    <Typography {...styles.detailsTypography2}> {info.postalCode} </Typography>
-                                </>
-                            )
-                        })}
-
-                    </Grid>
-                </Box>
-            </Grid>
-        </Card>
+        <Box>
+            <Typography variant="h5">Address 1</Typography>
+            <Card sx={{ mt: 3, mb: 3, p: 5 }}>
+                <Grid container spacing={3}>
+                    <Box {...styles.detailsParentBox} >
+                        <Grid item xs={12} md={2} lg={4}>
+                            <Typography {...styles.detailsTypography1}>Address Line 1 -</Typography>
+                            <Typography {...styles.detailsTypography1}>Address Line 2 -</Typography>
+                            <Typography {...styles.detailsTypography1}>Country -</Typography>
+                            <Typography {...styles.detailsTypography1}>State -</Typography>
+                            <Typography {...styles.detailsTypography1}>City -</Typography>
+                            <Typography {...styles.detailsTypography1}>Postal Code -</Typography>
+                        </Grid>
+                        <Grid item xs={12} md={10} lg={8}>
+                            <Typography {...styles.detailsTypography2}> {info.line1}</Typography>
+                            <Typography {...styles.detailsTypography2}> {info.line2} </Typography>
+                            <Typography {...styles.detailsTypography2}> {info.country} </Typography>
+                            <Typography {...styles.detailsTypography2}> {info.state} </Typography>
+                            <Typography {...styles.detailsTypography2}> {info.city} </Typography>
+                            <Typography {...styles.detailsTypography2}> {info.postalCode} </Typography>
+                        </Grid>
+                    </Box>
+                </Grid>
+            </Card>
+        </Box>
     )
 }
 
