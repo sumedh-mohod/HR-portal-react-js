02724cd5e084279031f71578b4f95a4825facf0f
kajal.nimje <kajal.nimje@orangebitsindia.com>
2023-01-24 19:20:22 +0530

    DocumentPersonal

--- src/components/Employee/Tabs/DocumentsPersonal.tsx
+++ src/components/Employee/Tabs/DocumentsPersonal.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
-
+import React from "react";
+import { Box, Typography, Card, Grid, Divider } from "@mui/material";
+import DocumentsPersonalCard from "./DocumentsPersonalCard";
+import { styles } from "../../../styles/components/employeeDocumentPersonalCard";
 const DocumentsPersonal = () => {
   return (
-    <div>
-      <h1>DocumentsPersonal</h1>
-    </div>
-  )
-}
+    <Box>
+      <Grid container spacing={2} sx={{ mt: 1 }}>
+        <Grid item xs={12} sm={12} md={6} lg={3} xl={3}>
+          <Typography {...styles.TopTypography} variant="h6">
+          Resume
+          </Typography>
+          <DocumentsPersonalCard  pdfTitle="Resume.PDF"/>
+        </Grid>
+        <Grid item xs={12} sm={12} md={6} lg={3} xl={3}>
+        <Typography {...styles.TopTypography} variant="h6">
+          Passport
+          </Typography>
+          <DocumentsPersonalCard pdfTitle="Passport.PDF"/>
+        </Grid>
+        <Grid item xs={12} sm={12} md={6} lg={3} xl={3}>
+        <Typography {...styles.TopTypography} variant="h6">
+        Aadhar Card
+          </Typography>
+          <DocumentsPersonalCard  pdfTitle="Aadhar Card.PDF" />
+        </Grid>
+        <Grid item xs={12} sm={12} md={6} lg={3} xl={3}>
+        <Typography {...styles.TopTypography} variant="h6">
+           PAN Card
+          </Typography>
+          <DocumentsPersonalCard pdfTitle="PAN Card.PDF"/>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
 
-export default DocumentsPersonal
+export default DocumentsPersonal;
