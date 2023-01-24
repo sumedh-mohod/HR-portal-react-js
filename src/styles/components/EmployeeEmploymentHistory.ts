c53fe19a0d19d297f41b367256e57ad58cf83f46
kajal.nimje <kajal.nimje@orangebitsindia.com>
2023-01-24 18:09:44 +0530

    employment history

---
+++ src/styles/components/EmployeeEmploymentHistory.ts
@@ -0,0 +1,86 @@
+export const styles = {
+  parentBox: {
+    sx: {
+      display: "flex",
+      justifyContent: "space-between",
+      mt: 2,
+    },
+  },
+  moduleTitle: {
+    sx: {
+      fontSize: "14px",
+      fontWeight: "600",
+      fontFamily: "Montserrat",
+      fontStyle: "normal",
+      color: "#F58634",
+      lineHeight: "27px",
+    },
+  },
+  parentBoxEditButton: {
+    sx: {
+      fontfamily: "Montserrat",
+      backgroundColor: "#DDE2FF",
+      borderRadius: "5px",
+      marginLeft: "10px",
+      fontStyle: "normal",
+      color: "#515151",
+      textTransform: "inherit",
+    },
+  },
+  icon: {
+    sx: { pr: 1, fontSize: "inheit" },
+  },
+
+  card: {
+    sx: {
+      mt: 3,
+      mb: 3,
+      p: 5,
+    },
+  },
+  textFieldLabel: {
+    sx: {
+      fontFamily: "Montserrat",
+      fontStyle: "normal",
+      fontWeight: "500",
+      fontSize: "14px",
+      lineHeight: "17px",
+      letterSpacing: "0.3px",
+      marginBottom: "10px",
+    },
+  },
+  deleteIconBox: {
+    sx: {
+      width: "38px",
+      height: "38px",
+      borderRadius: "5px",
+      backgroundColor: "#D82A2A",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      cursor: "pointer",
+    },
+  },
+  addButton: {
+    sx: {
+      marginLeft: "20px",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "48px",
+      paddingLeft: "15px",
+      paddingRight: "15px",
+      border: "2px dashed #C0C0C0",
+      boxShadow: "none",
+      color: "#1C1B1F",
+      borderRadius: "5px",
+    },
+  },
+  addIcon: {
+    sx: {
+      fontSize: "16px",
+      fontWeight: "600",
+      fontFamily: "Montserrat",
+      marginRight: 1,
+    },
+  },
+};
